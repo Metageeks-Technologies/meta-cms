6ae3281b4b921da712c0147de5664ee95041648b
Nitin Rawat <nitin.rawat@metageeks.tech>
2024-12-30 15:40:44 +0530

    Fixed signup page

--- client/src/utils/helperFunction.ts
+++ client/src/utils/helperFunction.ts
@@ -3,3 +3,11 @@ export const handleDate = (date: any) => {
     const newDate = new Date(date);
     return `${newDate.getDate()}/${newDate.getMonth() + 1}/${newDate.getFullYear()}`
 }
+
+export function isValidPassword(password: string) {
+    const hasUppercase = /[A-Z]/.test(password); 
+    const hasLowercase = /[a-z]/.test(password); 
+    const hasDigit = /\d/.test(password);       
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password); 
+    return hasUppercase && hasLowercase && hasDigit && hasSpecialChar;
+  }
